a0397fef8a79305c08995cb94e777605e513422e
unknown <kwame.boahene@live.com>
2026-05-05 21:10:50 +0100

    updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class AppHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"App Version 4 🚀")
+        self.wfile.write(b"App Version 4. Confirmation that app is working")
 
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 8000), AppHandler)
